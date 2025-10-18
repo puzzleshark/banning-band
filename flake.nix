27e6384ef0bdb5f10a6112ca6b4bf88426a3c94b
Samuel Banning <samcbanning@gmail.com>
2025-10-18 11:55:48 -0400

    first commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Banning Band website with Zola";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
+      buildInputs = [
+        nixpkgs.legacyPackages.x86_64-linux.zola
+      ];
+    };
+  };
+}
